79e7a255f487049b8a1bf4ce2a66d1b487e3bcdd
Millisami <millisami@gmail.com>
2013-05-27 21:22:20 +0545

    version upgraded to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "millisami@gmail"
 license          "Apache 2.0"
 description      "Installs/Configures MediaWiki"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "mysql"
 depends "apache2"
