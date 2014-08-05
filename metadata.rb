39efd2cf4438deb4352167099df15f08047bd7e3
Seth Thomas <sthomas@getchef.com>
2014-08-04 23:19:51 -0700

    Bumping to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'millisami@gmail'
 license          'Apache 2.0'
 description      'Installs/Configures MediaWiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'mysql'
 depends 'apache2'
