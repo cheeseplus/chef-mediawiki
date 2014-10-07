73c32c67ccdccde5098c93a6eaaf608241533763
Seth Thomas <cheeseplus@polycount.com>
2014-10-07 11:58:29 -0400

    Version 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'millisami@gmail'
 license          'Apache 2.0'
 description      'Installs/Configures MediaWiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'mysql'
 depends 'apache2'
